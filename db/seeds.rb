b8ac2c0039f8d7af8a8dea3d746ed885d7eb899b
woniesong92 <hs459@cornell.edu>
2017-04-01 22:52:13 -0700

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-# This will download 3 latest arvix papers from cs.AI category
-populate_with_real_papers(3)
-
 def populate_with_real_papers(max_results=5)
   raw_papers = ArvixGetter.new(max_results: max_results).papers
   hashed = Hash.from_xml(raw_papers)
@@ -10,3 +7,6 @@ def populate_with_real_papers(max_results=5)
     ArvixPaper::Create.new(entry).call
   end
 end
+
+# This will download 3 latest arvix papers from cs.AI category
+populate_with_real_papers(3)
